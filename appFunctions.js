801f4db1797e35f2e05943e9777454452228e701
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-04-01 17:34:11 +0200

    refactor appFunctions class to support asynchronous operations and enhance data handling; add HTML table structure for displaying data

--- appFunctions.js
+++ appFunctions.js
@@ -2,52 +2,76 @@ import { config } from './env.js'
 
 const dataRows = []
 const titleArray = []
+const tbody = document.querySelector('#tbody')
 
 export class appFunctions {
-    constructor() { 
+    constructor() {
+
     }
 
-    createScripts() {
+    async createScripts() {
         const scripts = [
-            'map.js',
+            `map.js`,
             `https://maps.googleapis.com/maps/api/js?key=${config.mapApiKey}&libraries=marker&callback=initMap&loading=async`
         ]
 
         for (let scriptsrc of scripts) {
             const script = document.createElement('script');
             script.src = scriptsrc;
+
             document.body.appendChild(script)
         }
     }
 
     createMarkers(result) {
-        return new Promise((resolve, reject) => {
-            for (let row = 1; row < result.length; row++) {
-                let obj = {}
-                for (let item = 0; item < titleArray.length; item++) {
-                    obj[titleArray[item]] = result[row][item]
-                }
-                dataRows.push(obj)
-                obj = {}
+
+        for (let row = 1; row < result.length; row++) {
+            let obj = {}
+            for (let item = 0; item < titleArray.length; item++) {
+                obj[titleArray[item]] = result[row][item]
             }
-            dataOfSpreadsheet.push(dataRows)
+            dataRows.push(obj)
+            obj = {}
+        }
+        dataOfSpreadsheet.push(dataRows)
+    }
+
+    createTitleArray(result) {
+        return new Promise((resolve, reject) => {
+            const title = result[0].values()
+            for (let item of title) {
+                titleArray.push(item)
+            }
+            dataOfSpreadsheet.push(titleArray)
             resolve()
         })
     }
 
-    createTitleArray(result) {
-
-        const title = result[0].values()
-        for (let item of title) {
-            titleArray.push(item)
-        }
-        dataOfSpreadsheet.push(titleArray)
-    }
-
     convertResult(result) {
-        this.createTitleArray(result)
-        this.createMarkers(result).then(this.createScripts())
+        this.createTitleArray(result).then(() => {
+            this.createMarkers(result)
+            this.writeToHTML()
+            this.createScripts()
+        })
 
     }
+    writeToHTML() {
+        const data = dataOfSpreadsheet[1].map(item => {
+            return { name: item.Name, city: item.City }
+        })
+        data.forEach(item => {
+            this.createTableRow(item)
+        })
+    }
+    createTableRow(data) {
+        const row = document.createElement('tr')
+        const cell1 = document.createElement('td')
+        const cell2 = document.createElement('td')
+        cell1.textContent = data.name
+        cell2.textContent = data.city
+        row.appendChild(cell1)
+        row.appendChild(cell2)
+        tbody.appendChild(row)
 
+    }
 }
